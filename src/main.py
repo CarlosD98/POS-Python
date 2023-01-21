b851309d5f7fa7d327cd3dbda8f975a026b03d65
CarlosD98 <carlos_dm98@hotmail.com>
2023-01-21 15:25:59 -0700

    claridad

--- src/main.py
+++ src/main.py
@@ -3,6 +3,10 @@ import pandas as pd
 from pandastable import Table
 
 window = tk.Tk()
+
+dataFolder="data/"
+DataSource = "fedex.csv"
+
 window.geometry('1600x900+10+10')
 window.title('Control de guias')
 label = tk.Label(text="CODIGO")
@@ -10,24 +14,29 @@ entry  = tk.Entry()
 label.pack()
 entry.pack()
 
-DataSource = pd.read_csv("data/fedex.csv")
+Data = pd.read_csv("data/fedex.csv")
 
 df = pd.DataFrame({
-    'Codigo':[],
-    'Nombre':[],
-    'Direccion':[],
-    'Celular':[]
+    'Codigo':["hola"],
+    'Nombre':['como'],
+    'Direccion':['estas'],
+    'Celular':['?']
 })
 
 frameTable = tk.Frame(window)
 frameTable.pack(fill='both',expand=True)
 
-pt = Table(frameTable, dataframe = df)
-pt.redraw()
-pt.show()
+table = Table(frameTable, dataframe = df)
+table.show()
 
 window.mainloop()
 
 def updateTable():
     df.add()
-    pt.redraw()
+    table.redraw()
+
+def reloadData():
+    Data = pd.read_csv(dataFolder+DataSource)
+
+def search():
+    res = Data.query()
